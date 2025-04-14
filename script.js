5b8123286e6af4932fc079b7343bbf180f9c3dbf
Pascal <pascalheilmann95@gmail.com>
2025-04-14 18:21:35 +1000

    added darkmode, added dynamic footer with js, added css vars for colours,  fix content view in experience fields, fix typo

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function set_current_year_footer(){
+  document.getElementById("footer").innerHTML = `Copyright &#169; ${new Date().getFullYear()} Pascal Heilmann. All Rights Reserved.`;
+}
+set_current_year_footer();
